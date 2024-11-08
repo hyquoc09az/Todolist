4d5789ad4089f9a43bddbecbd3e971c2a7d07350
Hồ Quốc Huy <148845523+hyquoc09az@users.noreply.github.com>
2024-11-08 12:10:02 +0700

    Add files via upload

---
+++ Todolist-main (1)/Todolist-main/Todolist/Todolist/Form1.cs
@@ -0,0 +1,20 @@
+namespace Todolist
+{
+    public partial class Todolist : Form
+    {
+        public Todolist()
+        {
+            InitializeComponent();
+        }
+
+        private void Todolist_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
